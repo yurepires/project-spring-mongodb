fc3f2de9fd212c2ebdab1669bbd2084dd08c8e4e
yurepires <contato.yurepires@gmail.com>
2025-03-10 15:14:27 -0300

    Consulta simples com @query

--- src/main/java/com/educandoweb/workshopmongo/services/PostService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/PostService.java
@@ -23,6 +23,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return postRepository.findByTitleContainingIgnoreCase(text);
+        return postRepository.findByTitle(text);
     }
 }
